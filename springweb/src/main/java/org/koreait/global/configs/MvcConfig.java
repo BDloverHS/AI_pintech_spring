dff725c9ddb0253f5b778b23d7972ce785463a32
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-11 11:25:55 +0900

    spring_web

--- springweb/src/main/java/org/koreait/global/configs/MvcConfig.java
+++ springweb/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // 기본적으로 해야할 설정들이 자동 추가
 @EnableWebMvc
 // 설정 통합
-@Import(Configuration.class)
+@Import(ControllerConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
